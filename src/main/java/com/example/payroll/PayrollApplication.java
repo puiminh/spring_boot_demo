06caef628fa5cfd16ccf25322b3d75842e6b213c
puiminh <volmicrolab@gmail.com>
2023-02-02 16:31:27 +0700

    fix version of mvn

--- src/main/java/com/example/payroll/PayrollApplication.java
+++ src/main/java/com/example/payroll/PayrollApplication.java
@@ -3,6 +3,11 @@ package com.example.payroll;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @SpringBootApplication //meta-annotation that pulls in component scanning, autoconfiguration, and property support
 public class PayrollApplication {
 
