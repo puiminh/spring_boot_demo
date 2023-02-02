20d02a55a65dee6ec0453283580ea0009d21d231
puiminh <volmicrolab@gmail.com>
2023-02-02 16:39:45 +0700

    fix instace

--- src/main/java/com/example/payroll/EmployeeRepository.java
+++ src/main/java/com/example/payroll/EmployeeRepository.java
@@ -3,6 +3,6 @@ package com.example.payroll;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface EmployeeRepository extends CrudRepository<Employee, Long> { //Declaring interface to accessing data
+public interface EmployeeRepository extends JpaRepository<Employee, Long> { //Declaring interface to accessing data
 
 }
